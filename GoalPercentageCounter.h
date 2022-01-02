92391f5268e31e25bc5f924932f1e85d999fea45
Timmeey86 <Gobbles86@users.noreply.github.com>
2022-01-03 00:27:50 +0100

    Removed unused template code

--- GoalPercentageCounter.h
+++ GoalPercentageCounter.h
@@ -10,10 +10,8 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class GoalPercentageCounter: public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow/*, public BakkesMod::Plugin::PluginWindow*/
+class GoalPercentageCounter: public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
-
-	//std::shared_ptr<bool> enabled;
 
 	//Boilerplate
 	virtual void onLoad();
@@ -23,26 +21,6 @@ class GoalPercentageCounter: public BakkesMod::Plugin::BakkesModPlugin, public B
 	void RenderSettings() override;
 	std::string GetPluginName() override;
 	void SetImGuiContext(uintptr_t ctx) override;
-
-	// Inherited via PluginWindow
-	/*
-
-	bool isWindowOpen_ = false;
-	bool isMinimized_ = false;
-	std::string menuTitle_ = "GoalPercentageCounter";
-
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
-	
-	*/
-
-
 
 private:
 
