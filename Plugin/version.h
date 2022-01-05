7aab722e3e89036be757f8a28baa39366d91d4aa
Timmeey86 <Gobbles86@users.noreply.github.com>
2022-01-05 11:35:28 +0100

    Moved plugin down a level, in order to add a unit test project later on

--- Plugin/version.h
+++ Plugin/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 4
 #define VERSION_PATCH 2
-#define VERSION_BUILD 98
+#define VERSION_BUILD 99
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
