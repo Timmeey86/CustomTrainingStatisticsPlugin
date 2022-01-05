2a67e77da2ffb517f6b33dc34eb6678048853509
Timmeey86 <Gobbles86@users.noreply.github.com>
2022-01-05 12:38:47 +0100

    Generated gtest/gmock and manually added projects to solution

--- Plugin/version.h
+++ Plugin/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 4
 #define VERSION_PATCH 2
-#define VERSION_BUILD 99
+#define VERSION_BUILD 100
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
