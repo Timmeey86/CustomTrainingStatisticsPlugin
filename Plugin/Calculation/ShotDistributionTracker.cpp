3e9cc1453221406091c0e78827604074dda4c871
Timmeey86 <Timmeey86@hotmail.com>
2022-02-26 22:44:32 +0100

    Restructures code and adds comments

--- Plugin/Calculation/ShotDistributionTracker.cpp
+++ Plugin/Calculation/ShotDistributionTracker.cpp
@@ -26,6 +26,7 @@ void ShotDistributionTracker::onTrainingModeLoaded(TrainingEditorWrapper& traini
 		}
 	}
 	_maximumValue = .0f;
+	_shotLocations.clear();
 }
 
 void ShotDistributionTracker::incrementHeatmapEntry(Vector ballLocation)
@@ -98,92 +99,109 @@ void ShotDistributionTracker::renderOneFrame(CanvasWrapper& canvas)
 {
 	canvas.SetPosition(Vector2{});
 
+	if (_heatMapIsVisible)
+	{
+		renderHeatMap(canvas);
+	}
+
+	if (_shotLocationsAreVisible)
+	{
+		renderShotLocations(canvas);
+	}
+}
+
+void ShotDistributionTracker::renderShotLocations(CanvasWrapper& canvas)
+{
+	canvas.SetColor(LinearColor{ 255.0f, 255.0f, 0.0f, 200.0f });
+
+	for (auto closeMissLocation : _shotLocations)
+	{
+		auto screenLocation = canvas.Project(closeMissLocation);
+		canvas.DrawRect(Vector2{ screenLocation.X - 5, screenLocation.Y - 5, }, Vector2{ screenLocation.X + 5, screenLocation.Y + 5 });
+	}
+}
+
+void ShotDistributionTracker::renderHeatMap(CanvasWrapper& canvas)
+{
 	for (auto x = 0; x < XBrackets; x++)
 	{
 		for (auto z = 0; z < ZBrackets; z++)
 		{
 			auto numberOfHitsInBracket = _heatmapData[x][z];
 
-			auto leftBorder = (float)(x * XBracketWidth - 4000);
-			auto rightBorder = leftBorder + XBracketWidth;
-			auto bottomBorder = (float)z * ZBracketHeight;
-			auto topBorder = bottomBorder + ZBracketHeight;
-
-
-			auto bottomLeft = Vector{ leftBorder, YDrawLocation, bottomBorder };
-			auto bottomRight = Vector{ rightBorder, YDrawLocation, bottomBorder };
-			auto topRight = Vector{ rightBorder, YDrawLocation, topBorder };
-			auto topLeft = Vector{ leftBorder, YDrawLocation, topBorder };
-
 			// If there are zero hits, don't paint it
 			if (numberOfHitsInBracket > 0)
 			{
-				// calculate the color like this:
-				// 0% of max value: black
-				// 20% of max value: pure Blue
-				// 40% of max value: blue + green (cyan)
-				// 60% of max value: pure green
-				// 80% of max value: green + red (yellow)
-				// 100% of max value: pure red
-				
-				auto percentageOfMaximum = numberOfHitsInBracket / _maximumValue;
-
-				// Red: 0%-60% = 0, 80%-100% = 1
-				// Green: 0-20% = 0, 40-60% = 1, 80-100% = 0
-				// Blue: 0% = 0, 20-40% = 1, 60-100% = 0
-				float redFactor, greenFactor, blueFactor;
-				if (percentageOfMaximum <= .2f)
-				{
-					redFactor = .0f;
-					greenFactor = .0f;
-					blueFactor = percentageOfMaximum * 5.0f; // Scale to 0-100
-				}
-				else if (percentageOfMaximum <= .4f)
-				{
-					redFactor = .0f;
-					greenFactor = (percentageOfMaximum - .2f) * 5.0f;
-					blueFactor = 1.0f;
-				}
-				else if (percentageOfMaximum <= .6f)
-				{
-					redFactor = .0f;
-					greenFactor = 1.0f;
-					blueFactor = 1.0f - ((percentageOfMaximum - .4f) * 5.0f);
-				}
-				else if (percentageOfMaximum <= .8f)
-				{
-					redFactor = (percentageOfMaximum - .6f) * 5.0f;
-					greenFactor = 1.0f;
-					blueFactor = .0f;
-				}
-				else
-				{
-					redFactor = 1.0f;
-					greenFactor = 1.0f - ((percentageOfMaximum - .8f) * 5.0f);
-					blueFactor = .0f;
-				}
-				auto heatColor = LinearColor{ 255.0f * redFactor, 255.0f * greenFactor, 255.0f * blueFactor, 50.0f + percentageOfMaximum * 200.0f };
-
-				auto bottomLeftProj = canvas.Project(bottomLeft);
-				auto bottomRightProj = canvas.Project(bottomRight);
-				auto topRightProj = canvas.Project(topRight);
-				auto topLeftProj = canvas.Project(topLeft);
-				canvas.SetColor(heatColor);
-
-				// We're using rectangles instead of triangles right now since triangles unfortunately ignore transparency
-				//canvas.FillTriangle(bottomLeftProj, topRightProj, bottomRightProj);
-				//canvas.FillTriangle(bottomLeftProj, topLeftProj, topRightProj);
-
-				canvas.DrawRect(bottomLeftProj, topRightProj);
+				canvas.SetColor(getHeatmapColor(numberOfHitsInBracket));
+				drawRectangle(x, z, canvas);
 			}
 		}
 	}
+}
 
-	//canvas.SetColor(LinearColor{ 255.0f, 255.0f, 0.0f, 200.0f });
+void ShotDistributionTracker::drawRectangle(int x, int z, CanvasWrapper& canvas)
+{
+	auto leftBorder = (float)(x * XBracketWidth - 4000);
+	auto rightBorder = leftBorder + XBracketWidth;
+	auto bottomBorder = (float)z * ZBracketHeight;
+	auto topBorder = bottomBorder + ZBracketHeight;
 
-	//for (auto closeMissLocation : _shotLocations)
-	//{
-	//	auto screenLocation = canvas.Project(closeMissLocation);
-	//	canvas.DrawRect(Vector2{ screenLocation.X - 5, screenLocation.Y - 5, }, Vector2{ screenLocation.X + 5, screenLocation.Y + 5 });
-	//}
+
+	auto bottomLeft = Vector{ leftBorder, YDrawLocation, bottomBorder };
+	auto topRight = Vector{ rightBorder, YDrawLocation, topBorder };
+
+	auto bottomLeftProj = canvas.Project(bottomLeft);
+	auto topRightProj = canvas.Project(topRight);
+
+	// Unfortunately we can't use triangles since these will ignore the alpha channel
+	canvas.DrawRect(bottomLeftProj, topRightProj);
+}
+
+LinearColor ShotDistributionTracker::getHeatmapColor(float numberOfHitsInBracket)
+{
+	// calculate the color like this:
+	// 0% of max value: black
+	// 20% of max value: pure Blue
+	// 40% of max value: blue + green (cyan)
+	// 60% of max value: pure green
+	// 80% of max value: green + red (yellow)
+	// 100% of max value: pure red
+
+	auto percentageOfMaximum = numberOfHitsInBracket / _maximumValue;
+
+	// Red: 0%-60% = 0, 80%-100% = 1
+	// Green: 0-20% = 0, 40-60% = 1, 80-100% = 0
+	// Blue: 0% = 0, 20-40% = 1, 60-100% = 0
+	float redFactor, greenFactor, blueFactor;
+	if (percentageOfMaximum <= .2f)
+	{
+		redFactor = .0f;
+		greenFactor = .0f;
+		blueFactor = percentageOfMaximum * 5.0f; // Scale to 0-100
+	}
+	else if (percentageOfMaximum <= .4f)
+	{
+		redFactor = .0f;
+		greenFactor = (percentageOfMaximum - .2f) * 5.0f;
+		blueFactor = 1.0f;
+	}
+	else if (percentageOfMaximum <= .6f)
+	{
+		redFactor = .0f;
+		greenFactor = 1.0f;
+		blueFactor = 1.0f - ((percentageOfMaximum - .4f) * 5.0f);
+	}
+	else if (percentageOfMaximum <= .8f)
+	{
+		redFactor = (percentageOfMaximum - .6f) * 5.0f;
+		greenFactor = 1.0f;
+		blueFactor = .0f;
+	}
+	else
+	{
+		redFactor = 1.0f;
+		greenFactor = 1.0f - ((percentageOfMaximum - .8f) * 5.0f);
+		blueFactor = .0f;
+	}
+	return LinearColor{ 255.0f * redFactor, 255.0f * greenFactor, 255.0f * blueFactor, 50.0f + percentageOfMaximum * 200.0f };
 }
--- Plugin/Calculation/ShotDistributionTracker.h
+++ Plugin/Calculation/ShotDistributionTracker.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <array>
+#include <vector>
 
 #include "../Core/AbstractEventReceiver.h"
 #include "../Core/IStatDisplay.h"
+
+#include <bakkesmod/wrappers/wrapperstructs.h>
 
 /** This class tracks where the ball touched the backboard.
  * Unlike the other classes, this one uses a single boolean flag instead of a state machine, since only two states are relevant.
@@ -12,25 +15,50 @@ class ShotDistributionTracker : public AbstractEventReceiver, public IStatDispla
 {
 
 public:
+	/** Creates a new object which keeps track of locations on the backboard or goal surface which were hit by the ball. */
 	explicit ShotDistributionTracker();
 
+	// Resets the tracked shot when a new pack gets loaded
 	void onTrainingModeLoaded(TrainingEditorWrapper& trainingWrapper, const std::string& trainingPackCode) override;
+	// Remembers the location of the goal which was hit and triggers a heatmap update
 	void onGoalScored(TrainingEditorWrapper& trainingWrapper, BallWrapper& ball) override;
+	// Remembers the location of the first wall hit (until the ball touches the ground or ceiling or car)
 	void onBallWallHit(TrainingEditorWrapper& trainingWrapper, BallWrapper& ball) override;
+	// The following three reset the flag which prevents further wall touches from being processed
 	void onBallGroundHit(TrainingEditorWrapper& trainingWrapper, BallWrapper& ball) override;
 	void onBallCeilingHit(TrainingEditorWrapper& trainingWrapper, BallWrapper& ball) override;
 	void onBallHit(TrainingEditorWrapper& trainingWrapper, bool isInitialHit) override;
 
+	// Renders the heat map and/or shot location overlays
 	void renderOneFrame(CanvasWrapper& canvas) override;
 
-	static const int XBrackets = 160;
-	static const int ZBrackets = 80;
+	
+	static const int XBrackets = 160; ///< Defines the number of brackets in X dimension. The number 8000 should be dividable by this number.
+	static const int ZBrackets = 80; ///< Defines the number of brackets in Z dimension. The number 4000 should be dividable by this number.
 
+	/** Shows or hides the heat map. */
+	inline void setHeatMapVisible(bool visible) { _heatMapIsVisible = visible; }
+	/** Shows or hides the shot locations. */
+	inline void setShotLocationsVisible(bool visible) { _shotLocationsAreVisible = visible; }
 
 private:
+	/** Increments the heat map entry at the shot location by 1 and slightly increments brackets around that location. */
 	void incrementHeatmapEntry(Vector ballLocation);
+	/** Draws the rectangle for the given cell. */
+	void drawRectangle(int x, int z, CanvasWrapper& canvas);
+	/** Retrieves the heatmap color for the given number of hits, in relation to the maximum value of all brackets. */
+	LinearColor getHeatmapColor(float numberOfHitsInBracket);
+	/** Renders the locations of actual impact points. */
+	void renderShotLocations(CanvasWrapper& canvas);
+	/** Renders the heat map. */
+	void renderHeatMap(CanvasWrapper& canvas);
+
 	std::array<std::array<float, ZBrackets>, XBrackets> _heatmapData; ///< Stores the number of hits in each cell
-	float _maximumValue = 0;
+	float _maximumValue = 0; ///< The maximum value of all brackets
+	bool _heatMapIsVisible = false; ///< Used for showing or hiding the heat map.
+
+	std::vector<Vector> _shotLocations; ///< Stores the locations of goals/bounces of the current training pack.
+	bool _shotLocationsAreVisible = false; ///< Used for showing or hiding the shot locations
 
 	bool _furtherWallHitsShallBeIgnored = false; ///< True while wall hits shall be ignored. This is necessary since rolling the ball up the wall would produce a myriad of hits.
 };
