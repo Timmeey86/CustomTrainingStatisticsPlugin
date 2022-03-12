763e6182ed0ca7b85a58d5aa8e404e38fc55661d
Timmeey86 <Timmeey86@hotmail.com>
2022-03-12 10:06:26 +0100

    Updates version number

--- Plugin/version.h
+++ Plugin/version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_PATCH 0
-#define VERSION_BUILD 976
+#define VERSION_PATCH 1
+#define VERSION_BUILD 977
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
