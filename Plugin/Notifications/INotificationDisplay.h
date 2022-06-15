7e06961aa8dbbc8aadf1ca008212e828c0957d32
Timmeey86 <Timmeey86@hotmail.com>
2022-06-15 22:05:55 +0200

    Added interface for decoupling the notification UI

---
+++ Plugin/Notifications/INotificationDisplay.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+/** Defines the interface for something which can display notifications. */
+class INotificationDisplay
+{
+public:
+	virtual ~INotificationDisplay() = default;
+
+protected:
+	INotificationDisplay() = default;
+
+	/** Registers a notification which could be displayed. */
+	virtual void registerNotification(const std::string& uniqueName, const std::string& title) = 0;
+
+	/** Display a registered notification using the given information. */
+	virtual void displayNotification(const std::string& uniqueName, const std::string& titleLine, const std::string& detailedInfo) = 0;
+};
